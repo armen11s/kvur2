e2cd926c19a38f9a2cc2b20e672cba25381287b7
armen11s <armen11_2001@rambler.ru>
2023-05-16 16:09:55 +0300

    добавил дискриминант. написал проверку условий

--- kvtest.py
+++ kvtest.py
@@ -6,3 +6,12 @@ while baddata == True:
         c = int(input('введите c:'))
     except:
         print('не удалось получить данные')
+
+D = (b*b) - (4 * a * c)
+if D > 0:
+      print('два кеорня')
+elif D == 0:
+    print('один корень')
+else:
+    print('нет корней')
+
