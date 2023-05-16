c56a76db938b84a7a9c22df8f9c0dc3c184831a7
armen11s <armen11_2001@rambler.ru>
2023-05-16 15:52:44 +0300

    добавил обработку ошибок

--- kvtest.py
+++ kvtest.py
@@ -1,4 +1,8 @@
-a = int(input('введите а:'))
-b = int(input('введите b:'))
-c = int(input('введите c:'))
-
+baddata = True
+while baddata == True:
+    try:
+        a = int(input('введите а:'))
+        b = int(input('введите b:'))
+        c = int(input('введите c:'))
+    except:
+        print('не удалось получить данные')
